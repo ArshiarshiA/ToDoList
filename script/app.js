49f5036bb259a8c49e28436cb36f7596ad068bba
arsian <marshia370@gmail.com>
2024-04-14 21:41:58 +0330

    clear button

--- script/app.js
+++ script/app.js
@@ -2,13 +2,22 @@
 let TaskList = document.querySelector('.taskList');
 let input = document.querySelector('input[type = text]');
 let button = document.querySelector('input[type = button]');
+let clear = document.querySelector('input[value = Clear]');
 
 // if clicked add button
 button.addEventListener('click' , ()=>{
-    let nameItem = input[0].value;
     // if input empty
-    if(!(nameItem == '')){
+    if(!(input.value == '')){
         let item = createNo('div' , nameItem , TaskList);
-        nameItem = '';
+        input.value = '';
+    }
+})
+
+// if clicked clear button
+clear.addEventListener('click' , ()=>{
+    // take all div in tasklist and removed
+    let items = document.querySelectorAll('.taskList > div');
+    for(let i = 0; i <= items.length; i++){
+        TaskList.removeChild(items[i]);
     }
 })
