4830cd0b56ebed6c9d6acc1f17723f7b1203ada2
arsian <marshia370@gmail.com>
2024-04-14 22:01:11 +0330

    if clicked trash icon and if clicked task

--- script/app.js
+++ script/app.js
@@ -8,7 +8,8 @@ let clear = document.querySelector('input[value = Clear]');
 button.addEventListener('click' , ()=>{
     // if input empty
     if(!(input.value == '')){
-        let item = createNo('div' , nameItem , TaskList);
+        let item = createNo('div' , input.value , TaskList);
+        item.innerHTML += '<span class="trash"><i class="fa fa-trash"></i></span>'
         input.value = '';
     }
 })
@@ -19,5 +20,17 @@ clear.addEventListener('click' , ()=>{
     let items = document.querySelectorAll('.taskList > div');
     for(let i = 0; i <= items.length; i++){
         TaskList.removeChild(items[i]);
+    }
+})
+
+
+TaskList.addEventListener('click' , (event)=>{
+    // if clicked delete icon
+    if(event.target.nodeName == 'I'){
+        event.target.parentNode.parentNode.style.display = 'none';
+    }
+    // if clicked task
+    if(event.target.nodeName == 'DIV'){
+        event.target.classList.toggle('done');
     }
 })
