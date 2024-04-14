c570be0d33cbfa579c6ff4b18dd26133abe98dda
arsian <marshia370@gmail.com>
2024-04-14 21:35:44 +0330

    add button

--- script/app.js
+++ script/app.js
@@ -2,3 +2,13 @@
 let TaskList = document.querySelector('.taskList');
 let input = document.querySelector('input[type = text]');
 let button = document.querySelector('input[type = button]');
+
+// if clicked add button
+button.addEventListener('click' , ()=>{
+    let nameItem = input[0].value;
+    // if input empty
+    if(!(nameItem == '')){
+        let item = createNo('div' , nameItem , TaskList);
+        nameItem = '';
+    }
+})
